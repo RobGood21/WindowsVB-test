6265f909bc47d336f48bbf4bd1ef2a05b8ca44aa
Rob Antonisse <robantonisse@gmail.com>
2017-03-23 10:47:19 +0100

    Connectie met SQLServer lukt niet

--- WindowsVB test/Artikel.vb
+++ WindowsVB test/Artikel.vb
@@ -1,12 +1,30 @@
-﻿Public Class Artikel
+﻿Option Explicit On
+Option Strict On
+
+Imports System.Data
+Imports System.Data.SqlClient
+
+
+
+
+Public Class Artikel
+
+
+
+
+
     Private Sub Artikel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' LijstP.Items.Add("Eerste regel")
         '  LijstP.Items.Add("Tweede regel")
+        'LijstP.DataSource = "select * from producten;"
+
 
 
     End Sub
 
     Private Sub KnopTest1_Click(sender As Object, e As EventArgs) Handles KnopTest1.Click
+        MsgBox("Hallo ik snap der niks van")
 
     End Sub
 End Class
+
